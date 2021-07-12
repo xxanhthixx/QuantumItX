43d15a723187906afefbf465554a94d84d8096eb
hungzero168 <47785168+hungzero168@users.noreply.github.com>
2021-07-12 20:56:08 +0700

    Update busuu.vip.js

--- busuu.vip.js
+++ busuu.vip.js
@@ -1,3 +1,9 @@
 var obj = JSON.parse($response.body);
 obj.data.is_premium= true;
+obj.data.access.tier= "plus";
 $done({body: JSON.stringify(obj)});
+
+
+#var obj = JSON.parse($response.body);
+#obj.data.is_premium= true;
+#$done({body: JSON.stringify(obj)});
